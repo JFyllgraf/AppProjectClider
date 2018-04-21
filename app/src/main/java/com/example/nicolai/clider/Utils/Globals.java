1e3ccd1d1b1bcaf15724de9523f7904011bb64d9
Nicolai Bomholt <nicolaibomholt@gmail.com>
2018-04-21 17:19:57 +0200

    Put some functionality in the background service

---
+++ app/src/main/java/com/example/nicolai/clider/Utils/Globals.java
@@ -0,0 +1,18 @@
+package com.example.nicolai.clider.Utils;
+
+public class Globals {
+    public static String cardSwipeMessage = "CARD_SWIPE_MESSAGE";
+    public static String clotheBroadcast = "CLOTHE_BROADCAST";
+
+    public static String shoeTag = "SHOE_TAG";
+    public static String jacketTag = "JACKET_TAG";
+    public static String dressTag = "DRESS_TAG";
+    public static String hatTag = "HAT_TAG";
+    public static String tshirtTag = "TSHIRT_TAG";
+    public static String shortsTag = "SHORTS_TAG";
+    public static String sportTag = "SPORT_TAG";
+    public static String accessoriesTag = "ACCESSORIES_TAG";
+
+
+
+}
